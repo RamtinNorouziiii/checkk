a8ca5157a4a9be27a43b8cbda957c5de0adaa24e
ramtinnorouziiii <rn.dejavoo@gmail.com>
2022-11-01 13:11:15 +0330

    build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   staticPageGenerationTimeout: 120,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
