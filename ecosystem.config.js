1944e0437cd7c4485e432c8105a4f0fee1e59f79
RamtinNorouziiii <rn.dejavoo@gmail.com>
2022-11-02 11:19:08 +0330

    slmmm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user : 'root',
       host : '192.168.103.23',
       ref  : 'origin/master',
-      repo : 'https://github.com/RamtinNorouziiii/mine',
+      repo : 'https://github.com/RamtinNorouziiii/checkk',
       path : '/etc/nginx/react',
       'pre-deploy-local': '',
       'post-deploy' : ' source ~/.nvm/nvm.sh &&  npm install && npm run build && pm2 reload ecosystem.config.js --env production',
